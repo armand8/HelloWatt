64fa9016d198b2da951fcb624f3abe5f21dd23a5
Armand <armand8@gmail.com>
2018-04-23 08:51:21 +0200

    annual costs + plot watt + is_elec_heating + is_disfunction

--- joole/dashboard/static/dashboard/js/results.js
+++ joole/dashboard/static/dashboard/js/results.js
@@ -1,1 +1,40 @@
-// ----> YOUR CODE HERE <----
+// Conso per year
+conso_euro = (JSON.parse(conso_euro));
+conso_euro.forEach(function(element) {
+  year = element[0];
+  element.shift();
+  sum = 0;
+  $.each(element,function(){sum+=parseFloat(this) || 0;});
+  sum = Math.round(sum);
+  $( ".annual_costs" ).append( "<p>Année : "+year+", dépense : "+sum+"€</p>" );
+});
+
+// Plot conso for 2017
+conso_watt = (JSON.parse(conso_watt));
+var options = {
+    series: {
+        lines: { show: true },
+        points: { show: true }
+    },
+    xaxes: [ {min: 1, max: 12 } ],
+};
+
+data = [[1, conso_watt[1][1]], [2, conso_watt[1][2]],[3, conso_watt[1][3]],[4, conso_watt[1][4]],[5, conso_watt[1][5]],
+[6, conso_watt[1][6]],[7, conso_watt[1][7]],[8, conso_watt[1][8]],[9, conso_watt[1][9]],[10, conso_watt[1][10]],
+[11, conso_watt[1][11]],[12, conso_watt[1][12]]];
+var dataset = [{label: "2017",data: data}];
+
+$(document).ready(function () {$.plot($("#flot-placeholder"),dataset, options);});
+
+//Is elec heating ?
+if (is_elec_heating == "True") {$( ".is_elec_heating" ).append( "<p>Oui</p>" );
+}else if (is_elec_heating == "False")  {
+    $( ".is_elec_heating" ).append( "<p>Non</p>" );
+}
+
+//Dysfunction_detected ?
+console.log(dysfunction_detected)
+if (dysfunction_detected == "True") {$( ".dysfunction_detected" ).append( "<p>Oui</p>" );
+}else if (dysfunction_detected == "False")  {
+    $( ".dysfunction_detected" ).append( "<p>Non</p>" );
+}
--- joole/dashboard/views.py
+++ joole/dashboard/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 from .forms import ClientForm
 from .models import Conso_eur, Conso_watt
@@ -16,21 +18,72 @@ class ClientFormView(View):
             client_id = form.cleaned_data['client']
             return redirect('dashboard:results', client_id=client_id)
 
+
 def results(request, client_id):
     conso_euro = []
     conso_watt = []
     annual_costs = [0, 0]
     is_elec_heating = True
     dysfunction_detected = False
+    conso_summer = 0
+    conso_winter = 0
+    year = 2017
+    is_elec_max_ratio = 1.1
+    conso_watt_per_year = []
+    annual_watt = []
+    dysfunction_detected_max_ratio = 1.1
 
-    ###################################
-    # ----> YOUR CODE GOES HERE <---- #
-    ###################################
+    # Conso_eur from db
+    conso_euro = Conso_eur.objects.filter(client_id=client_id).values_list('year', 'janvier', 'fevrier', 'mars', \
+                                                                            'avril', 'mai', 'juin', 'juillet', 'aout', \
+                                                                            'septembre', 'octobre', 'novembre',
+                                                                            'decembre')
+    conso_euro = json.dumps(list(conso_euro), cls=DjangoJSONEncoder)
+
+    # Conso_watt from db
+    conso_watt = Conso_watt.objects.filter(client_id=client_id).values_list('year', 'janvier', 'fevrier', 'mars', \
+                                                                            'avril', 'mai', 'juin', 'juillet', 'aout', \
+                                                                            'septembre', 'octobre', 'novembre',
+                                                                            'decembre')
+    conso_watt = json.dumps(list(conso_watt), cls=DjangoJSONEncoder)
+
+    # is_elec_heating
+    conso_summer = Conso_watt.objects.filter(client_id=client_id, year=year).values_list('juin', 'juillet', 'aout', \
+                                                                            'septembre')
+    conso_summer = json.dumps(list(conso_summer), cls=DjangoJSONEncoder)
+    conso_summer = sum(map(float, conso_summer.translate(None, '[]').split(',')))
+
+    conso_winter = Conso_watt.objects.filter(client_id=client_id, year=year).values_list('novembre', 'decembre', 'janvier', \
+                                                                            'fevrier')
+    conso_winter = json.dumps(list(conso_winter), cls=DjangoJSONEncoder)
+    conso_winter = sum(map(float, conso_winter.translate(None, '[]').split(',')))
+
+    if conso_winter/conso_summer > is_elec_max_ratio:
+        is_elec_heating = True
+    else:
+        is_elec_heating = False
+
+    # dysfunction_detected
+    for conso_watt_per_year in Conso_watt.objects.filter(client_id=client_id).values_list('year', 'janvier', 'fevrier', 'mars', \
+                                                                            'avril', 'mai', 'juin', 'juillet', 'aout', \
+                                                                            'septembre', 'octobre', 'novembre',
+                                                                            'decembre') :
+        conso_watt_per_year = json.dumps(list(conso_watt_per_year), cls=DjangoJSONEncoder)
+        conso_watt_per_year = map(float, conso_watt_per_year.translate(None, '[]').split(','))
+        conso_watt_per_year.pop(0)
+        conso_watt_per_year = sum(conso_watt_per_year)
+        annual_watt.append(conso_watt_per_year)
+    
+    if annual_watt[1]/annual_watt[0] > dysfunction_detected_max_ratio:
+        dysfunction_detected = True
+    else:
+        dysfunction_detected = False
+    print(annual_watt[1]/annual_watt[0])
 
     context = {
         "conso_euro": conso_euro,
         "conso_watt": conso_watt,
-        "annual_costs": annual_costs,
+        # "annual_costs": annual_costs,
         "is_elec_heating": is_elec_heating,
         "dysfunction_detected": dysfunction_detected
     }
